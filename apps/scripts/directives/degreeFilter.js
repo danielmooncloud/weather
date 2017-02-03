6bbec433160849cac203c26bd86edd2343b79961
Daniel Sousa <sousa.danielg@gmail.com>
2017-02-03 13:17:05 -0800

    blank.txt

--- apps/scripts/directives/degreeFilter.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var angular = require('angular');
-
-angular.module('weatherApp').filter('toCelsius', function() {
-	return function(number) {
-		return Math.floor((5/9) * (number - 32));
-	}
-});
